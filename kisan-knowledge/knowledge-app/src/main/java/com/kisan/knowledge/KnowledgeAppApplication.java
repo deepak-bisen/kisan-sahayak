d7309484f4200917d235b7b20b436f3b58fe4d54
Deepak14-12 <bisen1412@gmail.com>
2026-04-23 00:02:29 +0530

    updated

--- kisan-knowledge/knowledge-app/src/main/java/com/kisan/knowledge/KnowledgeAppApplication.java
+++ kisan-knowledge/knowledge-app/src/main/java/com/kisan/knowledge/KnowledgeAppApplication.java
@@ -3,9 +3,11 @@ package com.kisan.knowledge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan(basePackages = "com.kisan.knowledge")
 public class KnowledgeAppApplication
 {
     public static void main( String[] args )
--- kisan-marketplace/marketplace-app/src/main/java/com/kisan/marketplace/MarketplaceAppApplication.java
+++ kisan-marketplace/marketplace-app/src/main/java/com/kisan/marketplace/MarketplaceAppApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.kisan.marketplace") // This tells Spring to find your Feign Clients
+@ComponentScan(basePackages = "com.kisan.marketplace")
 public class MarketplaceAppApplication {
 
     public static void main(String[] args) {
